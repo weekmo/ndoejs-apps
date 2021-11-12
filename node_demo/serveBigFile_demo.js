515cbc257296aa9704fdc5fde16d9310937ca011
Mohammed Hassan <mohammed.hassan@basf.com>
2021-11-12 19:56:40 +0100

    change the structure

--- node_demo/serveBigFile_demo.js
+++ node_demo/serveBigFile_demo.js
@@ -2,7 +2,7 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer((req, res) => {
-    const fileStream = fs.createReadStream('./data/thirdBig.txt','utf8');
+    const fileStream = fs.createReadStream('../data/thirdBig.txt','utf8');
     fileStream.on('open', ()=> {
         fileStream.pipe(res);
     });
